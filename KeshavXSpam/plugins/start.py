9646c530a5fd7e541bf3aafc2110b315ae2f15b0
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-03 23:11:31 +0530

    Update start.py

--- KeshavXSpam/plugins/start.py
+++ KeshavXSpam/plugins/start.py
@@ -7,9 +7,9 @@ from .. import Mig, Mig2, Mig3, Mig4, Mig5, Mig6, Mig7, Mig8, Mig9, Mig10, ALIVE
 from KeshavXSpam.plugins.help import *
 
 
-MIG_IMG = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/a54c5f1aca50619d3374f.jpg"
+ALF_IMG = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/a54c5f1aca50619d3374f.jpg"
 
-Mig_Button = [
+Alf_Button = [
         [
         Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/@BTS_CHAT_ZONE")
         ],
@@ -18,7 +18,7 @@ Mig_Button = [
         ]
         ]
                
-MigX_Button = [
+AlfX_Button = [
         [
         Button.url("✨ ᴀᴄᴄᴏᴜɴᴛ ✨", "https://t.me/NotReallyAlpha"),
         Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/BTS_CHAT_ZONE")
@@ -45,24 +45,24 @@ MigX_Button = [
 @Mig10.on(events.NewMessage(pattern="/start"))
 async def start(event):              
     if event.is_private:
-       MigBot = await event.client.get_me()
-       bot_name = MigBot.first_name
-       bot_id = MigBot.id
+       AlfBot = await event.client.get_me()
+       bot_name = AlfBot.first_name
+       bot_id = AlfBot.id
        replied_user = await event.client(GetFullUserRequest(event.sender_id))
-       TheMighty = event.chat_id
+       TheKeshav = event.chat_id
        firstname = replied_user.user.first_name
        userid = replied_user.user.id
        ownermsg = f"**Hello Boss !!, It's Me {bot_name}, Your Spam Bot !! \n\n Click Below Buttons For Help. 🌚**"
        usermsg = f"**Hello !! [{firstname}](tg://user?id={userid})\nNice To Meet You, Well I Am [{bot_name}](tg://user?id={bot_id}), A Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Given Below.** \n\n**Powered By : [𝙆𝙀𝙎𝙃𝘼𝙑 𝙓 𝙎𝙥𝙖𝙢](https://t.me/BTS_CHAT_ZONE)**"
        if event.sender_id == OWNER_ID:
-            await event.client.send_file(TheMighty,
-                  MIG_IMG,
+            await event.client.send_file(TheKeshav,
+                  ALF_IMG,
                   caption=ownermsg, 
-                  buttons=Mig_Button)
+                  buttons=Alf_Button)
        else:
-            await event.client.send_file(TheMighty,
-                  MIG_IMG,
+            await event.client.send_file(TheKeshav,
+                  ALG_IMG,
                   caption=usermsg, 
-                  buttons=MigX_Button)
+                  buttons=AlfX_Button)
                 
 
